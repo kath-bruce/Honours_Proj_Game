e29774c7f041c822c3abf582182cc1585251f8bc
Kathleen Bruce <kath-bruce@hotmail.com>
2018-03-10 00:59:43 +0000

    Added video clips for tutorial and added video player for tutorial

---
+++ Honours Project Game/Assets/scripts/GifScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class GifScript : MonoBehaviour
+{
+    VideoPlayer gif;
+    RawImage display;
+
+    // Use this for initialization
+    void Start()
+    {
+        gif = GetComponent<VideoPlayer>();
+        display = GetComponent<RawImage>();
+
+        gif.Prepare();
+    }
+
+    void Update()
+    {
+        if (gif.isPrepared)
+            display.texture = gif.texture;
+
+        if (!gif.isPlaying)
+            gif.Play();
+    }
+}
